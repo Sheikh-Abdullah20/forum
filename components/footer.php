94d2d81f159a15554e0fc0f1e7090e6a105dd1d8
Sheikh-Abdullah20 <162795515+Sheikh-Abdullah20@users.noreply.github.com>
2024-06-20 13:18:36 +0500

    First Commit

---
+++ components/footer.php
@@ -0,0 +1,9 @@
+
+<div class="container-fluid bg-dark text-light">
+    <footer>
+        <p class="text-center">
+           This forum All Rights Reserved by Abdullah | till 2035 
+        </p>
+    </footer>
+</div>
+
