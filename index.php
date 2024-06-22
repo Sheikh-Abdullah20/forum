2a075b6eca3dd4ffaab84e3831534a2632cf24e1
Sheikh-Abdullah20 <162795515+Sheikh-Abdullah20@users.noreply.github.com>
2024-06-22 14:03:25 +0500

    carousel and cards images changed and images update functinality added

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
     <title>Forum - Coding</title>
 </head>
 
@@ -15,23 +16,35 @@ include "components/header.php";
 include "loginsystem/db_connect.php";
 ?>
 
+<?php
 
+// it will update the images when it change
+$image_path1 = "images/1.jpg";
+$image_url1 = $image_path1 . "?" . time();
+
+$image_path2 = "images/2.jpg";
+$image_url2 = $image_path2 . "?" . time();
+
+$image_path3 = "images/3.jpg";
+$image_url3 = $image_path3 . "?" . time();
+echo'
     <div class="container-fluid">
         <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
+            <div class="carousel-inner" style="height: 80vh; ">
                 <div class="carousel-item active">
-                    <img src="images/carousel1.jpg" class="d-block w-100" alt="...">
+                    <img src="'.$image_url1 .'" class="d-block w-100 img-fluid" alt="...">
                 </div>
                 <div class="carousel-item">
-                    <img src="images/carousel2.jpg" class="d-block w-100" alt="...">
+                    <img src="'.$image_url2.'" class="d-block w-100 img-fluid" alt="...">
                 </div>
                 <div class="carousel-item">
-                    <img src="images/carousel3.jpg" class="d-block w-100" alt="...">
+                    <img src="'.$image_url3.'" class="d-block w-100 img-fluid" alt="...">
                 </div>
             </div>
         </div>
-    </div>
+    </div>';
 
+    ?>
 
 
 
@@ -45,12 +58,14 @@ include "loginsystem/db_connect.php";
        $id = $rows['category_id'];
        $catname = $rows['category_name'];
        $catdesc = $rows['category_description'];
+       $image_path = "images/card_" . $catname . $id . ".jpg";
+       $image_url = $image_path . "?" . time();
         echo '
         <div class="col-md-4 my-2 d-flex justify-content-center">
         <div class="card" style="width: 18rem" >
-        <img src="images/card_'. $catname . $id .'.jpg" height = "180px" >
+        <img src="'.$image_url.'" height = "180px">
         <div class="card-body">
-        <h5 class="card-title"> <a class= "text-dark" href = "threadlist.php?catid= '. $id .  '"> '.   $catname . '</a></h5>
+        <h5 class="card-title"> <a class= "text-dark" href = "threadlist.php?catid= '. $id .'"> '.   $catname . '</a></h5>
         <p class="card-text">'. substr( $catdesc, 0, 80) .'...</p>
         <a href="threadlist.php?catid= '. $id .' " class= "btn btn-dark btn-outline-info text-light">View Threads</a>
     </div>
